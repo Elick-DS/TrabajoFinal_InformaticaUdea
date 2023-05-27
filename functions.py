e2adc6310fccd4d69b8d704088bb48d7bc253979
Juan Pereira <juan.pereira1@udea.edu.co>
2023-05-27 13:18:08 -0500

    1.3

--- functions.py
+++ functions.py
@@ -27,4 +27,27 @@ def ingresar_ubicacion():
     codigo_ubicacion = int(input("Ingrese el código de ubicación: "))
     nombre_ubicacion = input("Ingrese el nombre de la ubicación: ")
     piso = int(input("Ingrese el número de piso: "))
+
+def menu_principal():
+    while True:
+        print("Menú Principal")
+        print("1. Gestionar información equipos")
+        print("2. Gestionar información responsables")
+        print("3. Gestionar información ubicaciones")
+        print("4. Salir")
+
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            menu_equipos()
+        elif opcion == "2":
+            menu_responsables()
+        elif opcion == "3":
+            menu_ubicaciones()
+        elif opcion == "4":
+            print("¡Gracias por utilizar el sistema!")
+            break
+        else:
+            print("Error, ingrese una opción válida.")
+
     
