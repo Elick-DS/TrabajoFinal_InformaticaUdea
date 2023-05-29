e0f0344a2eebb1da92053f3b72b29700a9ad8ece
Juan Pereira <juan.pereira1@udea.edu.co>
2023-05-29 14:04:50 -0500

    1.8 ajustes
    Ajutes

--- Hola123.py
+++ Hola123.py
@@ -11,30 +11,6 @@ def ingreso_automatico_equipos():
     
     print("Ingreso satisfactorio.")
 
-def actualizar_equipo():
-    numero_activo = input("Ingrese el número de activo del equipo a actualizar: ")
-    
-    equipo = equipos_collection.find_one({"numero_activo": numero_activo})
-    if equipo:
-        nuevo_nombre = input("Ingrese el nuevo nombre del equipo: ")
-        nuevo_marca = input("Ingrese la nueva marca: ")
-        nuevo_codigo_ubicacion = input("Ingrese el nuevo código de ubicación: ")
-        nuevo_codigo_responsable = input("Ingrese el nuevo código de responsable: ")
-        
-        nuevo_equipo = {
-            "$set": {
-                "nombre": nuevo_nombre,
-                "marca": nuevo_marca,
-                "codigo_ubicacion": nuevo_codigo_ubicacion,
-                "codigo_responsable": nuevo_codigo_responsable
-            }
-        }
-        
-        equipos_collection.update_one({"numero_activo": numero_activo}, nuevo_equipo)
-        print("Equipo actualizado satisfactoriamente.")
-    else:
-        print("No se encontro el equipo.")
-
 
 def ver_equipos():
     equipos = equipos_collection.find()
--- data_base.py
+++ data_base.py
@@ -92,65 +92,9 @@ def eliminar_equipo():
 
 
 
-#Funciones creadas parcialmente
-import csv
-
-def ingreso_automatico_equipos():
-    archivo_csv = input("Aqui va el nombre del archivo CSV:")
-    
-    with open("InventarioIPS.csv", 'r') as csvfile:
-        equipos_csv = csv.DictReader(csvfile)
-        for equipo in equipos_csv:
-            mycol.insert_one(equipo)
-    
-    print("Ingreso satisfactorio.")
-
-def actualizar_equipo():
-    numero_activo = input("Ingrese el número de activo del equipo a actualizar: ")
-    
-    equipo = mycol.find_one({"numero_activo": numero_activo})
-    if equipo:
-        nuevo_nombre = input("Ingrese el nuevo nombre del equipo: ")
-        nuevo_marca = input("Ingrese la nueva marca: ")
-        while True:
-            bloque = input("Ingrese el bloque en el que se encuentra el dispositivo: ")
-            piso = input("Ingresa el piso en el que se encuentra el dispositivo: ")
-            nuevo_bp = f"B{bloque}P{piso}"
-            if nuevo_bp.strip() and nuevo_bp.isalnum():
-                break
-            else:
-                print("La marca del equipo no puede estar vacío, no puede contener caracteres especiales. Inténtelo nuevamente.")
-        nuevo_codigo_responsable = input("Ingrese el nuevo código de responsable: ")
-        
-        nuevo_equipo = {
-            "$set": {
-                "nombre_equipo": nuevo_nombre,
-                "marca": nuevo_marca,
-                "codigo_ubicacion": nuevo_bp,
-                "codigo_responsable": nuevo_codigo_responsable
-            }
-        }
-        
-        mycol.update_one({"numero_activo": numero_activo}, nuevo_equipo)
-        print("Equipo actualizado satisfactoriamente.")
-    else:
-        print("No se encontro el equipo.")
 
 
-def ver_equipos():
-    equipos = mycol.find()
-    if equipos.count() > 0:
-        print("Equipos registrados:")
-        for equipo in equipos:
-            print(f"Serial: {equipo['serial']}")
-            print(f"Número de activo: {equipo['numero_activo']}")
-            print(f"Nombre: {equipo['nombre_equipo']}")
-            print(f"Marca: {equipo['marca']}")
-            print(f"Código de ubicación: {equipo['codigo_ubicacion']}")
-            print(f"Código de responsable: {equipo['codigo_responsable']}")
-            print("----------")
-    else:
-        print("No hay equipos registrados.")
+
 
 
 
